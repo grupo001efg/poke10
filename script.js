bdc028c6556e6958f59469f1b267f580903bd47d
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-09-20 09:01:10 -0300

    versao 3 poke

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const buscaEntrada = document.getElementById('busca');
+const btnBusca = document.getElementById('btnBusca');
+const pokemon-info = document.getElementById('pokemon-info')
 
+
+// Busca o Pokemon pela API - PokeAPI
+async function busca(Pokemon) {
+    try {
+        //Busca os dados da API pelo nome ou número
+        const response = await fetch('https:/pokeapi.co/api/v2/pokemon/${pokemin.toLwerCAse()}');
+
+        if (response.ok) {
+            const data = await response.json();
+        }
+    }
+}
